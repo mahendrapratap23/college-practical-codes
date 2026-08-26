a6621618c1f1f8cf7db35524bade746dfd3f66f9
mahendrapratap23 <mp498343@gmail.com>
2026-08-26 12:48:03 +0530

    save change

---
+++ DAA/DAA_PRACTICAL_05.py
@@ -0,0 +1,51 @@
+# Practical 6 - 0/1 Knapsack using Dynamic Programming
+
+import time
+
+def knapsack(weights, values, capacity):
+
+    n = len(weights)
+
+    # Create table
+    dp = [[0] * (capacity + 1) for i in range(n + 1)]
+
+    # Check each item
+    for i in range(1, n + 1):
+
+        for bag in range(1, capacity + 1):
+
+            weight = weights[i - 1]
+            value = values[i - 1]
+
+            # If item can fit
+            if weight <= bag:
+
+                take = value + dp[i - 1][bag - weight]
+                skip = dp[i - 1][bag]
+
+                dp[i][bag] = max(take, skip)
+
+            # If item cannot fit
+            else:
+                dp[i][bag] = dp[i - 1][bag]
+
+    return dp[n][capacity]
+
+
+# Input
+weights = list(map(int, input("Enter weights: ").split()))
+values = list(map(int, input("Enter values: ").split()))
+capacity = int(input("Enter capacity: "))
+
+# Start time
+start = time.time()
+
+answer = knapsack(weights, values, capacity)
+
+# End time
+end = time.time()
+
+# Output
+print("Maximum value:", answer)
+print("Time Complexity: O(n * W)")
+print(f"Execution Time: {end - start:.6f} seconds")
