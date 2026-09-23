670358a0303d1b95c1323b435ce8db8530a142e1
mahendrapratap23 <mp498343@gmail.com>
2026-09-23 12:41:34 +0530

    file made

---
+++ DAA/DAA_PRACTICAL_08.py
@@ -0,0 +1,35 @@
+# Graph Traversal using DFS and BFS
+
+import time
+
+# Random graph
+graph = {
+    0: [1, 2],
+    1: [0, 3],
+    2: [0, 4],
+    3: [1],
+    4: [2]
+}
+
+# DFS
+def dfs(node, visited):
+    visited.add(node)
+    print(node, end=" ")
+
+    for neighbour in graph[node]:
+        if neighbour not in visited:
+            dfs(neighbour, visited)
+
+# BFS
+def bfs(start):
+    visited = {start}
+    queue = [start]
+
+    while queue:
+        node = queue.pop(0)
+        print(node, end=" ")
+
+        for neighbour in graph[node]:
+            if neighbour not in visited:
+                visited.add(neighbour)
+                queue.append(neighbour)
